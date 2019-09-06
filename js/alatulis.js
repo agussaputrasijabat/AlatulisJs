9893115bdfa4aecf893f6a765d06b677d4a0a876
Agus <agussaputrapro@live.com>
2019-09-06 13:43:02 +0700

    Fix eval

--- js/alatulis.js
+++ js/alatulis.js
@@ -1,57 +1,57 @@
-function RedeemPoint(Point, Timestamp) {
-	eval(`localStorage.redeempoint_${Timestamp} = '{IsLoading: true, Success: false}'`);
-	$.post('https://alatulis.com/modules/loyaltyrewardpoints/ajax.php?route=lrpfrontcheckoutcontroller&rand=' + Timestamp, {
+function RedeemPoint(Point) {
+	localStorage.redeempoint = `{IsLoading: true, Success: false}`;
+	$.post(`https://alatulis.com/modules/loyaltyrewardpoints/ajax.php?route=lrpfrontcheckoutcontroller&rand=` + Date.now(), {
 		action: "processredeempoints",
 		points: Point
 	}).done(function (res) {
-		eval(`localStorage.redeempoint_${Timestamp} = '{IsLoading: false, Success: true}'`);
+		localStorage.redeempoint = `{IsLoading: false, Success: true}`;
 	})
 		.fail(function (res) {
-			eval(`localStorage.redeempoint_${Timestamp} = '{IsLoading: false, Success: false}'`);
+			localStorage.redeempoint = `{IsLoading: false, Success: false}`;
 		})
 }
 
-function ClearRedeemPoint(Timestamp) {
-	eval(`localStorage.clearredeempoint_${Timestamp} = '{IsLoading: true, Success: false}'`);
-	$.post('https://alatulis.com/modules/loyaltyrewardpoints/ajax.php?route=lrpfrontcheckoutcontroller&rand=' + Timestamp, {
+function ClearRedeemPoint() {
+	localStorage.clearredeempoint = `{IsLoading: true, Success: false}`;
+	$.post(`https://alatulis.com/modules/loyaltyrewardpoints/ajax.php?route=lrpfrontcheckoutcontroller&rand=` + Date.now(), {
 		action: "processclearpoints"
 	}).done(function (res) {
-		eval(`localStorage.clearredeempoint_${Timestamp} = '{IsLoading: false, Success: true}'`);
+		localStorage.clearredeempoint = `{IsLoading: false, Success: true}`;
 	})
 		.fail(function (res) {
-			eval(`localStorage.clearredeempoint_${Timestamp} = '{IsLoading: false, Success: false}'`);
+			localStorage.clearredeempoint = `{IsLoading: false, Success: false}`;
 		})
 }
 
-function Login(Email, Password, Timestamp) {
-	eval(`localStorage.login_${Timestamp} = '{IsLoading: true, Success: false}'`);
+function Login(Email, Password) {
+	localStorage.login = `{IsLoading: true, Success: false}`;
 
 	$.ajax({
-		url: 'https://alatulis.com/login',
+		url: `https://alatulis.com/login`,
 		data: {
 			'email': Email,
 			'password': Password,
 			submitLogin: 1
 		},
 		type: "POST",
-		method: 'POST',
+		method: `POST`,
 		beforeSend: function (xhr) {
-			xhr.setRequestHeader('upgrade-insecure-requests', '1');
-			xhr.setRequestHeader('accept', 'text/html,application/xhtml+xml,application/xml;');
-			xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
+			xhr.setRequestHeader(`upgrade-insecure-requests`, `1`);
+			xhr.setRequestHeader(`accept`, `text/html,application/xhtml+xml,application/xml;`);
+			xhr.setRequestHeader(`content-type`, `application/x-www-form-urlencoded`);
 		},
 		success: function (res) {
-			var IsAuthenticated = res.indexOf('"isLogged":true') >= 0 ? true : false;
-			eval(`localStorage.login_${Timestamp} = '{IsLoading: false, Success: ${IsAuthenticated}}'`);
+			var IsAuthenticated = res.indexOf(`"isLogged":true`) >= 0 ? true : false;
+			localStorage.login = `{IsLoading: false, Success: ${IsAuthenticated}}`;
 		},
 		error: function () {
-			Login(Email, Password, Timestamp);
+			Login(Email, Password);
 		}
 	});
 }
 
-function Register(Gender, Firstname, Lastname, Email, Password, Timestamp) {
-	eval("localStorage.register_" + Timestamp + " = '{IsLoading: true, Success: false}'");
+function Register(Gender, Firstname, Lastname, Email, Password) {
+	localStorage.register = `{IsLoading: true, Success: false}`;
 	$.ajax({
 		url: "https://alatulis.com/login?create_account=1",
 		data: {
@@ -64,39 +64,39 @@ function Register(Gender, Firstname, Lastname, Email, Password, Timestamp) {
 			"submitCreate": "1"
 		},
 		type: "POST",
-		method: 'POST',
+		method: `POST`,
 		beforeSend: function (xhr) {
-			xhr.setRequestHeader('ContentType', 'application/x-www-form-urlencoded');
+			xhr.setRequestHeader(`ContentType`, `application/x-www-form-urlencoded`);
 		},
 		success: function (res) {
-			var IsAuthenticated = res.indexOf('"isLogged":true') >= 0 ? true : false;
-			eval(`localStorage.register_${Timestamp} = '{IsLoading: false, Success: ${IsAuthenticated}}'`);
+			var IsAuthenticated = res.indexOf(`"isLogged":true`) >= 0 ? true : false;
+			localStorage.register = `{IsLoading: false, Success: ${IsAuthenticated}}`;
 		},
 		error: function () {
-			$.get('https://alatulis.com/akun-saya', function (res) {
-				var IsAuthenticated = res.indexOf('"isLogged":true') >= 0 ? true : false;
-				eval(`localStorage.register_${Timestamp} = '{IsLoading: false, Success: ${IsAuthenticated}}'`);
+			$.get(`https://alatulis.com/akun-saya`, function (res) {
+				var IsAuthenticated = res.indexOf(`"isLogged":true`) >= 0 ? true : false;
+				localStorage.register = `{IsLoading: false, Success: ${IsAuthenticated}}`;
 			});
 		}
 	});
 }
 
-function Cart(Timestamp) {
-	eval("localStorage.cart_" + Timestamp + "=''");
+function Cart() {
+	localStorage.cart = ``;
 
-	$.post('https://alatulis.com/keranjang-belanja?rand=' + Timestamp, {
+	$.post(`https://alatulis.com/keranjang-belanja?rand=${Date.now()}`, {
 		"ajax": 1,
 		"action": "update"
 	}, function (res) {
 		var result = JSON.parse(res);
-		eval("localStorage.cart_" + Timestamp + "='" + JSON.stringify(result.cart) + "'");
+		localStorage.cart = `${JSON.stringify(result.cart)}`;
 	});
 }
 
-function AddToCart(ProductId, Quantity, Timestamp) {
-	eval("localStorage.cart_" + Timestamp + "=''");
+function AddToCart(ProductId, Quantity) {
+	localStorage.cart = ``;
 	$.ajax({
-		url: "https://alatulis.com/keranjang-belanja?rand=" + Timestamp,
+		url: "https://alatulis.com/keranjang-belanja?rand=" + Date.now(),
 		data: {
 			"token": prestashop.static_token,
 			"id_product": ProductId,
@@ -106,19 +106,19 @@ function AddToCart(ProductId, Quantity, Timestamp) {
 			"action": "update"
 		},
 		type: "POST",
-		method: 'POST',
+		method: `POST`,
 		beforeSend: function (xhr) {
-			xhr.setRequestHeader('accept', 'application/json');
+			xhr.setRequestHeader(`accept`, `application/json`);
 		},
 		success: function (res) {
 			var result = JSON.parse(res);
-			eval("localStorage.cart_" + Timestamp + "='" + JSON.stringify(result.cart) + "'");
+			localStorage.cart = `${JSON.stringify(result.cart)}`;
 		}
 	});
 }
 
-function QuantityUp(ProductId, Timestamp) {
-	eval("localStorage.cart_qty_" + Timestamp + "=''");
+function QuantityUp(ProductId) {
+	localStorage.cart_qty = ``;
 	$.ajax({
 		url: "https://alatulis.com/keranjang-belanja?update=1&id_product=" + ProductId + "&id_product_attribute=0&token=" + prestashop.static_token + "&op=up",
 		data: {
@@ -126,19 +126,19 @@ function QuantityUp(ProductId, Timestamp) {
 			"action": "update"
 		},
 		type: "POST",
-		method: 'POST',
+		method: `POST`,
 		beforeSend: function (xhr) {
-			xhr.setRequestHeader('accept', 'application/json');
+			xhr.setRequestHeader(`accept`, `application/json`);
 		},
 		success: function (res) {
 			var result = JSON.parse(res);
-			eval("localStorage.cart_qty_" + Timestamp + "='" + JSON.stringify(result.cart) + "'");
+			localStorage.cart_qty = `${JSON.stringify(result.cart)}`;
 		}
 	});
 }
 
-function QuantityDown(ProductId, Timestamp) {
-	eval("localStorage.cart_qty_" + Timestamp + "=''");
+function QuantityDown(ProductId) {
+	localStorage.cart_qty = ``;
 	$.ajax({
 		url: "https://alatulis.com/keranjang-belanja?update=1&id_product=" + ProductId + "&id_product_attribute=0&token=" + prestashop.static_token + "&op=down",
 		data: {
@@ -146,19 +146,19 @@ function QuantityDown(ProductId, Timestamp) {
 			"action": "update"
 		},
 		type: "POST",
-		method: 'POST',
+		method: `POST`,
 		beforeSend: function (xhr) {
-			xhr.setRequestHeader('accept', 'application/json');
+			xhr.setRequestHeader(`accept`, `application/json`);
 		},
 		success: function (res) {
 			var result = JSON.parse(res);
-			eval("localStorage.cart_qty_" + Timestamp + "='" + JSON.stringify(result.cart) + "'");
+			localStorage.cart_qty = `${JSON.stringify(result.cart)}`;
 		}
 	});
 }
 
-function DeleteFromCart(ProductId, Timestamp) {
-	eval("localStorage.cart_qty_" + Timestamp + "=''");
+function DeleteFromCart(ProductId) {
+	localStorage.cart_qty = ``;
 	$.ajax({
 		url: "https://alatulis.com/keranjang-belanja?delete=1&id_product=" + ProductId + "&id_product_attribute=0&token=" + prestashop.static_token,
 		data: {
@@ -166,13 +166,13 @@ function DeleteFromCart(ProductId, Timestamp) {
 			"action": "update"
 		},
 		type: "POST",
-		method: 'POST',
+		method: `POST`,
 		beforeSend: function (xhr) {
-			xhr.setRequestHeader('accept', 'application/json');
+			xhr.setRequestHeader(`accept`, `application/json`);
 		},
 		success: function (res) {
 			var result = JSON.parse(res);
-			eval("localStorage.cart_qty_" + Timestamp + "='" + JSON.stringify(result.cart) + "'");
+			localStorage.cart_qty = `${JSON.stringify(result.cart)}`;
 		}
 	});
 }
@@ -182,7 +182,7 @@ function GetCustomerPoint() {
 		localStorage.customer_point = "";
 
 	$.get("https://alatulis.com/module/loyaltyrewardpoints/customeraccount", function (res) {
-		var Point = $(res).find('#lrp-customer-account .points-card span:first').text();
+		var Point = $(res).find(`#lrp-customer-account .points-card span:first`).text();
 		localStorage.customer_point = Point;
 	});
 }
@@ -197,17 +197,17 @@ function InitCustomerPoint() {
 }
 
 function GetWishlists() {
-	localStorage.wishlist = '';
-	$.get('https://alatulis.com/module/blockwishlist/mywishlist', function (res) {
+	localStorage.wishlist = ``;
+	$.get(`https://alatulis.com/module/blockwishlist/mywishlist`, function (res) {
 		var wishlist = [];
-		var $table = $(res).find('#block-history table');
-		$table.find('tbody tr').each(function (i, tr) {
-			var id = parseInt($(tr).attr('id').split('_')[1]);
-			var name = $(tr).find('td').eq(0).text().trim();
-			var quantity = parseInt($(tr).find('td').eq(1).text().trim());
-			var seen = parseInt($(tr).find('td').eq(2).text().trim());
-			var created_at = $(tr).find('td').eq(3).text().trim();
-			var is_default = $(tr).find('td').eq(5).find('.is_wish_list_default').length > 0;
+		var $table = $(res).find(`#block-history table`);
+		$table.find(`tbody tr`).each(function (i, tr) {
+			var id = parseInt($(tr).attr(`id`).split(`_`)[1]);
+			var name = $(tr).find(`td`).eq(0).text().trim();
+			var quantity = parseInt($(tr).find(`td`).eq(1).text().trim());
+			var seen = parseInt($(tr).find(`td`).eq(2).text().trim());
+			var created_at = $(tr).find(`td`).eq(3).text().trim();
+			var is_default = $(tr).find(`td`).eq(5).find(`.is_wish_list_default`).length > 0;
 
 			wishlist.push({
 				id: id,
@@ -221,99 +221,99 @@ function GetWishlists() {
 
 		localStorage.wishlist = JSON.stringify(wishlist);
 	}).fail(function () {
-		localStorage.wishlist = '[]';
+		localStorage.wishlist = `[]`;
 	});
 }
 
 function GetWishlistDetail(WishlistId) {
-	eval("localStorage.wishlist_" + WishlistId + " = ''");
+	localStorage.wishlist = ``;
 	$.get(`https://alatulis.com/modules/blockwishlist/managewishlist.php?rand=${Date.now()}&id_wishlist=${WishlistId}&refresh=false&`, function (res) {
 		var products = [];
-		var $ul = $(res).find('ul.wlp_bought_list');
-		$ul.find('li').each(function (i, li) {
-			var $id = $(li).attr('id').split('_')[1];
+		var $ul = $(res).find(`ul.wlp_bought_list`);
+		$ul.find(`li`).each(function (i, li) {
+			var $id = $(li).attr(`id`).split(`_`)[1];
 			products.push({ id: $id });
 		});
 
-		eval("localStorage.wishlist_" + WishlistId + " = '" + JSON.stringify(products) + "'");
+		localStorage.wishlist = `${JSON.stringify(products)}`;
 	}).fail(function () {
-		eval("localStorage.wishlist_" + WishlistId + " = '[]'");
+		localStorage.wishlist = `[]`;
 	});
 }
 
 function SetDefaultWishlist(WishlistId) {
-	eval(`localStorage.default_wishlist_${WishlistId} = '{IsLoading: true, Success: false}'`);
+	localStorage.default_wishlist = `{ IsLoading: true, Success: false }`;
 	$.post(`https://alatulis.com/wishlist?rand=${Date.now()}&default=1&id_wishlist=${WishlistId}&myajax=1&action=setdefault`)
 		.done(function (res) {
-			eval(`localStorage.default_wishlist_${WishlistId} = '{IsLoading: false, Success: true}'`);
+			localStorage.default_wishlist = `{ IsLoading: false, Success: true }`;
 		})
 		.fail(function (res) {
-			eval(`localStorage.default_wishlist_${WishlistId} = '{IsLoading: false, Success: false}'`);
+			localStorage.default_wishlist = `{ IsLoading: false, Success: false }`;
 		})
 }
 
 function DeleteWishlist(WishlistId) {
-	eval(`localStorage.delete_wishlist_${WishlistId} = '{IsLoading: true, Success: false}'`);
+	localStorage.delete_wishlist = `{ IsLoading: true, Success: false }`;
 	$.post(`https://alatulis.com/wishlist?rand=${Date.now()}&deleted=1&myajax=1&id_wishlist=${WishlistId}&action=deletelist`)
 		.done(function (res) {
-			eval(`localStorage.delete_wishlist_${WishlistId} = '{IsLoading: false, Success: true}'`);
+			localStorage.delete_wishlist = `{ IsLoading: false, Success: true }`;
 		})
 		.fail(function (res) {
-			eval(`localStorage.delete_wishlist_${WishlistId} = '{IsLoading: false, Success: false}'`);
+			localStorage.delete_wishlist = `{ IsLoading: false, Success: false }`;
 		})
 }
 
 function CreateWishlist(Name) {
 	var Token = prestashop.token;
-	$.post('https://alatulis.com/module/blockwishlist/mywishlist', {
+	$.post(`https://alatulis.com/module/blockwishlist/mywishlist`, {
 		token: Token,
 		name: Name,
-		submitWishlist: 'Simpan'
+		submitWishlist: `Simpan`
 	}, function (res) {
-		var $table = $(res).find('#block-history table');
+		var $table = $(res).find(`#block-history table`);
 		var isNameFound = false;
-		$table.find('tbody tr').each(function (i, tr) {
-			var name = $(tr).find('td').eq(0).text().trim();
+		$table.find(`tbody tr`).each(function (i, tr) {
+			var name = $(tr).find(`td`).eq(0).text().trim();
 			if (name == Name) isNameFound = true;
 		});
 
-		eval(`localStorage.create_wishlist_${Name} = '{IsLoading: false, Success: ${isNameFound}}'`);
+		localStorage.create_wishlist = `{ IsLoading: false, Success: ${isNameFound}}`;
 	}).fail(function () {
-		eval(`localStorage.create_wishlist_${Name} = '{IsLoading: false, Success: false}'`);
+		localStorage.create_wishlist = `{ IsLoading: false, Success: false }`;
 	});
 }
 
 function DeleteProductFromWishlist(WishlistId, ProductId) {
-	eval(`localStorage.delete_product_wishlist_${WishlistId} = '{IsLoading: true, Success: false}'`);
+	localStorage.delete_product_wishlist = `{ IsLoading: true, Success: false }`;
 	$.post(`https://alatulis.com/modules/blockwishlist/managewishlist.php?rand=${Date.now()}&action=delete&id_wishlist=${WishlistId}&id_product=${ProductId}&id_product_attribute=0&quantity=1&priority=1&refresh=true`)
 		.done(function (res) {
 			var isSuccess = true;
-			var $ul = $(res).find('ul.wlp_bought_list');
-			$ul.find('li').each(function (i, li) {
-				var id = $(li).attr('id').split('_')[1];
+			var $ul = $(res).find(`ul.wlp_bought_list`);
+			$ul.find(`li`).each(function (i, li) {
+				var id = $(li).attr(`id`).split(`_`)[1];
 				if (id == ProductId) isSuccess = false;
 			});
-			eval(`localStorage.delete_product_wishlist_${WishlistId} = '{IsLoading: false, Success: ${isSuccess}}'`);
+			localStorage.delete_product_wishlist = `{ IsLoading: false, Success: ${isSuccess}}`;
 		})
 		.fail(function (res) {
-			eval(`localStorage.delete_product_wishlist_${WishlistId} = '{IsLoading: false, Success: false}'`);
+			localStorage.delete_product_wishlist = `{ IsLoading: false, Success: false }`;
 		})
 }
 
 function AddProductToWishlist(WishlistId, ProductId) {
-	eval(`localStorage.add_product_wishlist_${WishlistId} = '{IsLoading: true, Success: false}'`);
+	localStorage.add_product_wishlist = `{ IsLoading: true, Success: false }`;
 	$.post(`https://alatulis.com/modules/blockwishlist/cart.php?rand=1567487066161&action=add&id_product=10&quantity=1&token=15fda96f28bfc4864e137cfecb6cc8b3&id_product_attribute=false&id_wishlist=null`)
 		.done(function (res) {
 			var isSuccess = true;
-			var $ul = $(res).find('ul.wlp_bought_list');
-			$ul.find('li').each(function (i, li) {
-				var id = $(li).attr('id').split('_')[1];
+			var $ul = $(res).find(`ul.wlp_bought_list`);
+			$ul.find(`li`).each(function (i, li) {
+				var id = $(li).attr(`id`).split(`_`)[1];
 				if (id == ProductId) isSuccess = false;
 			});
-			eval(`localStorage.add_product_wishlist_${WishlistId} = '{IsLoading: false, Success: ${isSuccess}}'`);
+			localStorage.add_product_wishlist = `{ IsLoading: false, Success: ${isSuccess}}`
 		})
 		.fail(function (res) {
-			eval(`localStorage.add_product_wishlist_${WishlistId} = '{IsLoading: false, Success: false}'`);
+			localStorage.add_product_wishlist = `{ IsLoading: false, Success: false }`;
 		})
 }
 
